0b00851d3d9c5ffae0d454cc62749d54354743af
James <320james@gmail.com>
2022-06-30 15:49:49 -0400

    cors

--- server/server.js
+++ server/server.js
@@ -20,10 +20,7 @@ const cors = require('cors');
 
 
 const app = express();
-app.use(cors({
-  origin: "https://cheers-application.netlify.app",
-  credentials: true,
-}))
+app.use(cors());
 
 
 // Init Middleware
